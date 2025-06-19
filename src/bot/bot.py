4feb3e4f733a81be6af6f27929f5b3cd3112a44c
Eric Joseph <contato.ericjoseph@gmail.com>
2025-06-19 04:34:04 +0000

    refactor: change some environment variable names

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,5 +1,3 @@
-
-
 from io import BytesIO
 from uuid import uuid4
 from aiogram import Bot, Dispatcher
@@ -10,15 +8,21 @@ from aiogram.types import BufferedInputFile
 from helpers import getenv
 
 TELEGRAM_BOT_API_KEY = getenv("TELEGRAM_BOT_API_KEY")
-OWNER_USER_ID = getenv("OWNER_USER_ID")
+OWNER_USER_ID = getenv("OWNER_ID")
 
 dp = Dispatcher()
-bot = Bot(token=TELEGRAM_BOT_API_KEY, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(
+    token=TELEGRAM_BOT_API_KEY, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
 
 async def send_file(buffer: BytesIO, filename: str | None):
     buffer.seek(0)
-    input_file = BufferedInputFile(buffer.read(), filename=filename if filename is not None else str(uuid4()) )
+    input_file = BufferedInputFile(
+        buffer.read(), filename=filename if filename is not None else str(uuid4())
+    )
     await bot.send_document(chat_id=OWNER_USER_ID, document=input_file)
+
 
 async def run_telegram_bot():
     await dp.start_polling(bot)
